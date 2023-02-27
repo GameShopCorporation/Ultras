e65f62f700350fb699d57f63948a3958816c708a
Lynden Evans <lyndenjayevans@gmail.com>
2023-02-26 20:18:28 -0500

    For Now, This Is Not The Point.  I Probably Will Not Delete The Comments.

--- app/src/main/java/com/gameshopllc/ultras/AppMain.java
+++ app/src/main/java/com/gameshopllc/ultras/AppMain.java
@@ -1,6 +1,10 @@
 package com.gameshopllc.ultras;
 import com.jme3.app.SimpleApplication;
 import com.jme3.math.ColorRGBA;
+import com.jme3.math.FastMath;
+import com.jme3.math.Matrix3f;
+import com.jme3.math.Quaternion;
+import com.jme3.math.Vector3f;
 import com.jme3.renderer.RenderManager;
 import com.jme3.scene.Geometry;
 
@@ -50,10 +54,53 @@ public class AppMain extends SimpleApplication {
 
            // this.cam.lookAt(lyndenJayEvans.allCircles.get(0).vertices.get(0), Vector3f.UNIT_Y); Wrong Equation, Freezes Camera
 
+            /*
+You can interpolate rotations between two quaternions using spherical linear
+interpolation (slerp).
+*/
+
+
             for (BigCircle circle: lyndenJayEvans.allCircles) {
 
-                float theCoinFlip = circle.poly.geo.getLocalTranslation().y + this.cam.getDirection().y;
-                circle.poly.geo.rotate(0, theCoinFlip, 0);
+
+//                Quaternion Xroll45 = new Quaternion();
+//                Xroll45.fromAngleAxis(this.cam.getDirection().x * FastMath.DEG_TO_RAD, Vector3f.UNIT_X);
+////
+//                Quaternion Yroll45 = new Quaternion();
+//                Yroll45.fromAngleAxis(this.cam.getDirection().y * FastMath.DEG_TO_RAD, Vector3f.UNIT_Y);
+//
+//                Quaternion Zroll45 = new Quaternion();
+//                Zroll45.fromAngleAxis(this.cam.getDirection().z * FastMath.DEG_TO_RAD, Vector3f.UNIT_Y);
+////
+//
+////the rotation half - way between these two
+//
+//                Quaternion halfBetweenXroll45Yroll45 = new Quaternion();
+//
+//                Quaternion halfBetweenYroll45Zroll45 = new Quaternion();
+//
+//                Quaternion halfBetweenZroll45Xroll45 = new Quaternion();
+//
+//                halfBetweenXroll45Yroll45.slerp(Xroll45, Yroll45, 0f);
+//                halfBetweenYroll45Zroll45.slerp(Yroll45, Zroll45, 0f);
+//                halfBetweenZroll45Xroll45.slerp(Zroll45, Xroll45, 0f);
+
+
+                //rotate a spatial to face up ~45 degrees
+//                Vector3f[] axes = new Vector3f[3];
+//                axes[0] = new Vector3f(0, 0, 0); //left
+//                axes[1] = new Vector3f(0, 0.0f, 0.0f); //up
+//                axes[2] = new Vector3f(0, 0.0f, 0.0f); //dir
+
+               // axes.getLocalRotation().fromAxes(axes);
+
+                //rotate 1 radian on the x, 3 on the y and 0 on z
+                float[] angles = {0, 1, 0};
+                //.getLocalRotation().fromAngles(angles);
+
+               // float theCoinFlip = this.cam.getDirection().y;
+                circle.poly.geo.getLocalRotation().fromAngles(angles);
+               // rotate(0, theCoinFlip - this.cam.getDirection().y, 0);
 
             }
         } else {
